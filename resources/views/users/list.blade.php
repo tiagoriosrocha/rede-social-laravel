1f330ae42c08d6a2c7cf99032e32cb516f682b73
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2023-10-19 10:29:34 -0300

    listando usuarios com botoes de seguir e remover

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        @foreach($listaUsuarios as $umUser)
+            <div class="card mb-3 col-md-7">
+                <div class="card-body">
+                    <a href="/user/{{ $umUser->id }}" class="link-underline-light">
+                        {{$umUser->name}}
+                    </a>
+                    @if($usuarioAutenticado->follows->contains($umUser))
+                        <button type="button" class="btn btn-outline-primary btn-sm float-end">Seguir</button>
+                    @else
+                        <button type="button" class="btn btn-outline-danger btn-sm float-end">Remover</button>
+                    @endif
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
